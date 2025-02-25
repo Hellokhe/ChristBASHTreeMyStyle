18c5b08b87332668991b9ace139ef2ce4f08564d
Hellokhe <141879109+Hellokhe@users.noreply.github.com>
2025-02-25 15:45:48 -0500

    Update tree-EN.sh

--- tree-EN.sh
+++ tree-EN.sh
@@ -27,13 +27,13 @@ tput sgr0; tput setaf 3
 for ((i=1; i<=2; i++))
 {
     tput cup $((lin++)) $c
-    echo 'mWm'
+    echo 'WWW'
 }
 new_year=$(date +'%Y')
 let new_year++
 tput setaf 1; tput bold
-tput cup $lin $((c - 6)); echo MERRY CHRISTMAS
-tput cup $((lin + 1)) $((c - 10)); echo And lots of CODE in $new_year
+tput cup $lin $((c - 6)); echo MERRY CHRISTMAS (:
+tput cup $((lin + 1)) $((c - 10)); echo And Lots Of CODE in $new_year
 let c++
 k=1
 
